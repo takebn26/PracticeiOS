2bd5ad53e326710efa5f6085b8df774ca1cbfed7
takebn26 <takehaya1026@gmail.com>
2018-06-13 01:22:05 +0900

    initial commit

---
+++ PracticeiOS/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  PracticeiOS
+//
+//  Created by Hayato Takemoto on 2018/06/12.
+//  Copyright © 2018年 Hayato Takemoto. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("applicationが立ち上がったよ")
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("applicationがアクティブじゃなくなったよ")
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       print("applicationがバックグラウンドに入ったよ")
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("applicationがフォアグラウンドに入ったよ")
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("applicationがアクティブになったよ")
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("applicationが終了したよ")
+    }
+}
+
