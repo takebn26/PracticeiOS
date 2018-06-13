fef3bdf4d8052fdc14de7a005a725e33a7af9ea6
竹本隼人 <takehaya1026@gmail.com>
2018-06-13 14:51:41 +0900

    plus

---
+++ PracticeiOS/View2Controller.swift
@@ -0,0 +1,23 @@
+//
+//  View2Controller.swift
+//  PracticeiOS
+//
+//  Created by 竹本隼人 on 2018/06/13.
+//  Copyright © 2018年 Hayato Takemoto. All rights reserved.
+//
+
+import UIKit
+
+class View2Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- PracticeiOS/ViewController.swift
+++ PracticeiOS/ViewController.swift
@@ -9,18 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pageLabel: UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "画面タイトル"
+        self.title = "画面タイトル"
         
-        view.backgroundColor = .white
+        self.view.backgroundColor = .white
+        let labelFrame = CGRect(x: 20, y: 80, width: view.bounds.size.width, height: 20)
         
-        let pageLabel = UILabel(frame: CGRect(x: 20, y: 80, width: view.bounds.size.width, height: 20))
-        pageLabel.backgroundColor = .red
-        pageLabel.text = "ViewController"
-        self.view.addSubview(pageLabel)
+        pageLabel = UILabel(frame: labelFrame)
+        
+        if let `pageLabel` = pageLabel {
+            pageLabel.backgroundColor = .clear
+            pageLabel.text = "ViewController"
+            view.addSubview(pageLabel)
+        }
+        
+        
+        let nbutton = UIButton(type: .system)
+        nbutton.frame = CGRect(x: 224, y: 340, width: 73, height: 44)
+        nbutton.backgroundColor = .gray
+        nbutton.setTitle("Next", for: .normal)
+        nbutton.setTitleColor(.white, for: .normal)
+        nbutton.addTarget(self, action: #selector(self.onNext(_:)), for: .touchUpInside)
+        
+        view.addSubview(nbutton)
+        
+        let rbutton = UIButton(type: .system)
+        rbutton.frame = CGRect(x: 124, y: 400, width: 73, height: 44)
+        rbutton.backgroundColor = .gray
+        rbutton.setTitle("Remove", for: .normal)
+        rbutton.setTitleColor(.white, for: .normal)
+        rbutton.addTarget(self, action: #selector(self.onRemove(_:)), for: .touchUpInside)
+        
+        view.addSubview(rbutton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +52,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func onNext(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "ViewController2", bundle: nil)
+        let view2Controller = storyboard.instantiateViewController(withIdentifier: "ViewController3")
+        view.addSubview(view2Controller.view)
+    }
+    
+    @objc fileprivate func onRemove(_ sender: UIButton) {
+        guard let pageLabel = pageLabel else {
+            return
+        }
+        pageLabel.removeFromSuperview()
+    }
 }
 
